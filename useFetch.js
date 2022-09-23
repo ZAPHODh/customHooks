5e1a8f0cd95a9960cf517b38f3cd813c7174236c
LUIS PAULO MARTINS <luispaulo.ni@gmail.com>
2022-09-23 14:07:15 -0300

    making the useFetch better

---
+++ useFetch.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = (url, options) => {
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    console.log('EFFECT', new Date().toLocaleString());
+
+    setLoading(true);
+
+    const fetchData = async () => {
+      await new Promise((r) => setTimeout(r, 3000));
+
+      try {
+        const response = await fetch(url, options);
+        const jsonResult = await response.json();
+        setResult(jsonResult);
+        setLoading(false);
+      } catch (e) {
+        setLoading(false);
+        throw e;
+      }
+    };
+
+    fetchData();
+  }, [url, options]);
+
+  return [result, loading];
+};
+export default useFetch;
